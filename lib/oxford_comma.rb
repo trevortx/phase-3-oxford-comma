69de198505795e5a0e4f7084a4de5ca4c1b755a6
Trevor Vardeman <trevortx@gmail.com>
2022-06-13 00:05:12 -0700

    commit tree 5df1bf9d4d3491b438b2bdf07d3e10aa3a5cd7ea

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  filler = "and "
+  case array.length
+  when 1
+    array.join
+  when 2
+    array.join(" #{filler}")
+  else
+    new_array = array
+    last_index = "and " + array[-1]
+    new_array.pop()
+    new_array << last_index
+    new_array.join(", ")
+  end
 end
